5f8b68be1f27c990455f029120a1cbc76a9baac8
Athar Naveed <asphaltlegends24@gmail.com>
2023-08-04 15:09:51 +0500

    displayed the word if the user fails

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random,pygame
+import random,pygame,os
 class Hangman():
     def __init__(self):
         pygame.init()
@@ -15,7 +15,8 @@ class Hangman():
         self.word_list=[("food","cake"),("place","park"),("animals","lion"),("celestial","star"),("transportation","ship"),("sports","ball"),("celestial","moon"),("weather","rain"),("aquatic","fish"),("nature","tree")]
     # Greetings function
     def greetings(self):
-        print("welcome to the hangman game")
+        print("Welcome to the hangman game")
+        self.play_sound("music/Success2.wav")
     
     # Playing sounds
     def play_sound(self,audio):
@@ -27,11 +28,11 @@ class Hangman():
         number = random.randint(0,9)
         self.catagory,self.word=self.word_list[number]  #Storing the category and the word in their respective variables
         self.splitted = list(self.word) #Splitting the word
-        print(f"\n {self.catagory}\t\tScore:{self.score}\t\tLives:{self.lives}\n") #Displaying score and lives
-        for dash in self.splitted:
-                print(" _",end=" ")
+        print(f"\n\t\t\t {self.catagory}\t\tScore:{self.score}\t\tLives:{self.lives}\n") #Displaying score and lives
+        #Printing the dash list
+        for dash in self.dash:
+                print("\t\t\t",dash,end=" ")
         print("\n")
-        
         for char in self.qwerty: #Displaying the keyboard here
             print(char,end=" ")
             if char == "p" or char == "l" or char == "m":
@@ -47,7 +48,7 @@ class Hangman():
     
     #Repeating Layout 
     def layout(self,index,word):
-        print(f"\n {self.catagory}\t\tScore:{self.score}\t\tLives:{self.lives}\n")
+        print(f"\n\t\t\t {self.catagory}\t\tScore:{self.score}\t\tLives:{self.lives}\n\t\t\t\t") #Displaying score and lives
         if word in self.splitted:
             #Popping the dashes from the specified index and adding the word at that index
             for i in index:
@@ -83,7 +84,8 @@ class Hangman():
     
     #Taking user input here
     def user_input(self):
-        word = input("Guess the word: ")
+        word = input("\nGuess the word: ")
+        os.system("cls")
         indices = self.checker(word)
         self.layout(indices,word)
 
@@ -166,13 +168,15 @@ class Hangman():
     def announce(self):
         if self.stored == self.splitted:
             print("\nYEAH! You WON!")
+            self.play_sound("music/Success2.wav")
             print(f"Your score is: {self.score}")
 
         else:
+            self.play_sound("music/Fail.wav")
+            print("The word was: ",self.word)
             print("\nBetter luck next time :(") 
           
 #Main
 h = Hangman()
 h.greetings()
 h.get_random_words()
-
