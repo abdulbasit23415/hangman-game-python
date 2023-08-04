78c89fbec644d9892282418d0c303bcd34ff4c06
Athar Naveed <asphaltlegends24@gmail.com>
2023-08-04 15:10:18 +0500

    show word when the player loses

--- main.py
+++ main.py
@@ -28,10 +28,10 @@ class Hangman():
         number = random.randint(0,9)
         self.catagory,self.word=self.word_list[number]  #Storing the category and the word in their respective variables
         self.splitted = list(self.word) #Splitting the word
-        print(f"\n\t\t\t {self.catagory}\t\tScore:{self.score}\t\tLives:{self.lives}\n") #Displaying score and lives
+        print(f"\n {self.catagory}\t\tScore:{self.score}\t\tLives:{self.lives}\n") #Displaying score and lives
         #Printing the dash list
         for dash in self.dash:
-                print("\t\t\t",dash,end=" ")
+                print(dash,end=" ")
         print("\n")
         for char in self.qwerty: #Displaying the keyboard here
             print(char,end=" ")
@@ -48,7 +48,7 @@ class Hangman():
     
     #Repeating Layout 
     def layout(self,index,word):
-        print(f"\n\t\t\t {self.catagory}\t\tScore:{self.score}\t\tLives:{self.lives}\n\t\t\t\t") #Displaying score and lives
+        print(f"\n {self.catagory}\t\tScore:{self.score}\t\tLives:{self.lives}\n") #Displaying score and lives
         if word in self.splitted:
             #Popping the dashes from the specified index and adding the word at that index
             for i in index:
